26666aa6ecb100912513d2dbce1ae67120197913
Scarlett <scarlettxoliveira@gmail.com>
2023-09-13 09:17:11 -0300

    🎨 Remoção do context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+
 const { schema } = require("./graphql/schema.js");
 
 const server = new ApolloServer({
   schema,
 });
-
 
 async function startServerAsync() {
   const { url } = await startStandaloneServer(server, {
@@ -20,5 +20,5 @@ startServerAsync()
     console.log(`Server started at ${url}`);
   })
   .catch((error) => {
-    console.error('Error starting the server:', error);
+    console.error("Error starting the server:", error);
   });
