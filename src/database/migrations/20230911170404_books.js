d1bb6c2e019eb8c8cb9c820c42d7a53dde26692f
Scarlett <scarlettxoliveira@gmail.com>
2023-09-14 11:46:41 -0300

    🎨 Alterações referentes ao comentário sobre a função .notNullable()

--- src/database/migrations/20230911170404_books.js
+++ src/database/migrations/20230911170404_books.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("books", (table) => {
     table.increments("id").primary();
-    table.string("title").notNullable;
-    table.string("author").notNullable;
-    table.string("image").notNullable;
-    table.decimal("dateOfRelease").notNullable;
-    table.decimal("numberOfPages").notNullable;
+    table.string("title").notNullable();
+    table.string("author").notNullable();
+    table.string("image").notNullable();
+    table.decimal("dateOfRelease").notNullable();
+    table.decimal("numberOfPages").notNullable();
   });
 };
 
--- src/database/migrations/20230913130024_reviews.js
+++ src/database/migrations/20230913130024_reviews.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reviews", (table) => {
     table.increments("id").primary();
-    table.string("title").notNullable;
-    table.string("author").notNullable;
-    table.string("book").notNullable;
-    table.string("review").notNullable;
-    table.decimal("dateOfRelease").notNullable;
+    table.string("title").notNullable();
+    table.string("author").notNullable();
+    table.string("book").notNullable();
+    table.string("review").notNullable();
+    table.decimal("dateOfRelease").notNullable();
   });
 };
 
